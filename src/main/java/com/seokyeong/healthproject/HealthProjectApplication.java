257705338ef62b2c81d6161b53b344b73e44e1ed
kariseio <kariseio@naver.com>
2022-05-25 18:10:17 +0900

    기본 세팅

---
+++ src/main/java/com/seokyeong/healthproject/HealthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.seokyeong.healthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthProjectApplication.class, args);
+	}
+
+}
